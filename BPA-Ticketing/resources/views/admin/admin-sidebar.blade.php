4a4ad9551c5b5d32e70002829554713dd052f08f
Yohana <yohanameilyawati07@gmail.com>
2026-06-25 02:48:32 +0700

    update sidebar admin

--- BPA-Ticketing/resources/views/admin/admin-sidebar.blade.php
+++ BPA-Ticketing/resources/views/admin/admin-sidebar.blade.php
@@ -6,24 +6,24 @@
 
     <ul class="sidebar-menu">
 
-        <li class="{{ request()->is('admin/admin-dashboard') ? 'active' : '' }}">
-            <a href="/admin/admin-dashboard">Beranda</a>
+        <li class="{{ request()->is('admin/dashboard') ? 'active' : '' }}">
+            <a href="/admin/dashboard">Beranda</a>
         </li>
 
-        <li class="{{ request()->is('admin/admin-ticket') ? 'active' : '' }}">
-            <a href="/admin/admin-ticket">Daftar Tiket</a>
+        <li class="{{ request()->is('admin/tickets') ? 'active' : '' }}">
+            <a href="/admin/tickets">Daftar Tiket</a>
         </li>
 
-        <li class="{{ request()->is('admin/admin-kategori') ? 'active' : '' }}">
-            <a href="/admin/admin-kategori">Kategori</a>
+        <li class="{{ request()->is('admin/kategori') ? 'active' : '' }}">
+            <a href="/admin/kategori">Kategori</a>
         </li>
 
-        <li class="{{ request()->is('admin/admin-pengguna') ? 'active' : '' }}">
-            <a href="/admin/admin-pengguna">Pengguna</a>
+        <li class="{{ request()->is('admin/pengguna') ? 'active' : '' }}">
+            <a href="/admin/pengguna">Pengguna</a>
         </li>
 
-        <li class="{{ request()->is('admin/admin-laporan') ? 'active' : '' }}">
-            <a href="/admin/admin-laporan">Laporan</a>
+        <li class="{{ request()->is('admin/laporan') ? 'active' : '' }}">
+            <a href="/admin/laporan">Laporan</a>
         </li>
 
     </ul>
